b279474e8cddf145b50639395c7675d3e1b1a39a
João Rafael Quintiliano de Souza <joao@directd.com.br>
2023-09-29 23:56:01 -0300

    Foi criado o arquivo do teste de hoje.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+    if (($i % 3) == 0)
+        echo 'R2 <hr>';
+    else if (($i % 5) == 0)
+        echo 'Sis <hr>';
+    else
+        echo $i.' <hr>';
+}
